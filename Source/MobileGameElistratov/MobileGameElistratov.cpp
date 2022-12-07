84d51d8f3381daa8d8cf8d573197719de962ac33
elistratovvm <vitalii.elistratov@gmail.com>
2022-12-07 11:08:57 +0300

    Initial Commit

---
+++ Source/MobileGameElistratov/MobileGameElistratov.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MobileGameElistratov.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MobileGameElistratov, "MobileGameElistratov" );
